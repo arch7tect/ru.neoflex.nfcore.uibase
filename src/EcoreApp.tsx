91f28223130c3c0ecdf196a7c24882981c669581
arch7tect <arch7tect@gmail.com>
2019-06-30 11:24:36 +0300

    cleanup: separate query and error handling

--- src/EcoreApp.tsx
+++ src/EcoreApp.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Table } from 'antd';
+import {Table} from 'antd';
 import 'antd/dist/antd.css';
 import {Ecore} from "ecore";
+import {Resource} from './modules/resource'
 
 //const Ecore =  require('ecore');
 
@@ -18,92 +19,18 @@ export class EcoreApp extends React.Component<Props, State> {
         this.state = {}
     }
 
-    fetchJson(input: RequestInfo, init?: RequestInit): Promise<any> {
-        return fetch(input, init).then(response=>{
-            if (!response.ok) {
-                response.json().then(json => {
-                    console.log(json.message, response.statusText);
-                }).catch(error => {
-                    console.log(response.statusText);
-                })
-                throw Error()
-            }
-            return response
-        }).catch(error => {
-            if (error && error.message) {
-                console.log(error.message);
-            }
-            return Promise.reject()
-        }).then(response => response.json());
-    }
-
-    fetchEObject(ref: string, level: number = 1, resourceSet?: Ecore.ResourceSet, loading?: any): Promise<any> {
-        loading = loading || {}
-        let rs = resourceSet || Ecore.ResourceSet.create();
-
-        let resid = ref.split('#')[0];
-        let refid = resid.split('?')[0];
-
-        if (loading.hasOwnProperty(refid)) {
-            return loading[refid];
-        }
-
-        function collectReferences(object: any, found: string[]): string[] {
-            if (typeof object !== 'object') {
-                return found;
-            }
-            let ref = object.$ref;
-            if (ref) {
-                found.push(ref);
-            }
-            for(var i in object) {
-                if(object.hasOwnProperty(i)){
-                    collectReferences(object[i], found);
-                }
-            }
-            return found;
-        }
-
-        let result =  this.fetchJson(`/emf/object?ref=${refid}`).then(json=>{
-            let refEObjects: Promise<any>[] = []
-            if (level > 0) {
-                let refs = collectReferences(json, []);
-                refEObjects = refs.map(ref=>{
-                    return this.fetchEObject(ref, level - 1, rs);
-                })
-            }
-            return Promise.all(refEObjects).then(resources=>{
-                let resource = rs.create({ uri: resid });
-                resource.load(json);
-                let eObject = resource.get('contents').first();
-                return eObject;
-            })
-        })
-        loading[refid] = result;
-        return result;
-    }
-
     componentDidMount(): void {
-        this.fetchJson("/emf/packages").then(json => {
-            let resourceSet = Ecore.ResourceSet.create();
-            for (let aPackage of json as any[]) {
-                let uri = aPackage['nsURI'];
-                let resource = resourceSet.create({ uri });
-                resource.load(aPackage);
-                resource.get('contents').each((ePackage: Ecore.EPackage) => {
-                    Ecore.EPackage.Registry.register(ePackage);
-                })
-            }
+        Resource.instance().fetchPackages().then(packages=>{
             this.setState({loaded: true})
         })
         let id: string = '59bb208006149f50bb32f76f4901029b';
-        this.fetchEObject(id, 999).then(eObject=>{
+        Resource.instance().fetchEObject(id, 999).then(eObject => {
             console.log(eObject);
         })
     }
 
     render() {
-        let getId = (eObject: any):  string => {
+        let getId = (eObject: any): string => {
             //if (eObject._id) return eObject._id;
             return eObject.eURI();
         }
@@ -115,8 +42,7 @@ export class EcoreApp extends React.Component<Props, State> {
                 let isContainment = Boolean(eObject.get('containment')) === true;
                 if (isContainment) {
                     prefix = prefix + 'contains ';
-                }
-                else {
+                } else {
                     prefix = prefix + 'refers ';
                 }
                 let eReferenceType = eObject.get('eType');
@@ -136,8 +62,7 @@ export class EcoreApp extends React.Component<Props, State> {
                 let upperBound = eObject.get('upperBound')
                 if (upperBound && upperBound !== 1) {
                     prefix = prefix + '[] ';
-                }
-                else {
+                } else {
                     prefix = prefix + ' ';
                 }
             }
@@ -153,13 +78,12 @@ export class EcoreApp extends React.Component<Props, State> {
                 }
                 if (eObject.get('interface')) {
                     prefix = prefix + 'interface ';
-                }
-                else {
+                } else {
                     prefix = prefix + 'class ';
                 }
                 let eSuperTypes = (eObject.get('eSuperTypes') as any[]).filter(e => e.get('name') !== 'EObject')
                 if (eSuperTypes.length > 0) {
-                    postfix = " extends " + eSuperTypes.map(e=>e.get('name')).join(", ")
+                    postfix = " extends " + eSuperTypes.map(e => e.get('name')).join(", ")
                 }
             }
             if (eObject.isKindOf('EOperation')) {
@@ -167,11 +91,8 @@ export class EcoreApp extends React.Component<Props, State> {
                 if (eType) {
                     prefix = eType.get('name') + ' ';
                 }
-                else {
-                    console.log(eObject);
-                }
                 let eParameters = eObject.get('eParameters') as any[]
-                postfix = '(' + eParameters.map(p=>{
+                postfix = '(' + eParameters.map(p => {
                     return p.get('eType').get('name') + ' ' + p.get('name')
                 }).join(', ') + ')';
             }
@@ -179,28 +100,49 @@ export class EcoreApp extends React.Component<Props, State> {
         }
         let data: any[] = []
         for (let ePackage of Ecore.EPackage.Registry.ePackages()) {
-            let children: any[] = []
-            data.push({key: getId(ePackage), name: getName(ePackage), type: ePackage.eClass.get('name'), children})
+            let eClassifiers: any[] = []
+            data.push({key: getId(ePackage), name: getName(ePackage), type: ePackage.eClass.get('name'), children: eClassifiers})
             for (let eClassifier of ePackage.get('eClassifiers').array()) {
                 let children2: any[] = []
-                children.push({key: getId(eClassifier), name: getName(eClassifier), type: eClassifier.eClass.get('name'), children: children2});
+                let child = {
+                    key: getId(eClassifier),
+                    name: getName(eClassifier),
+                    type: eClassifier.eClass.get('name'),
+                    children: children2
+                };
+                eClassifiers.push(child);
                 let eStructuralFeatures = eClassifier.get('eStructuralFeatures');
                 if (eStructuralFeatures) {
                     for (let eStructuralFeature of eStructuralFeatures.array()) {
-                        children2.push({key: getId(eStructuralFeature), name: getName(eStructuralFeature), type: eStructuralFeature.eClass.get('name')})
+                        children2.push({
+                            key: getId(eStructuralFeature),
+                            name: getName(eStructuralFeature),
+                            type: eStructuralFeature.eClass.get('name')
+                        })
                     }
                 }
                 let eLiterals = eClassifier.get('eLiterals');
                 if (eLiterals) {
                     for (let eLiteral of eLiterals.array()) {
-                        children2.push({key: getId(eLiteral), name: eLiteral.get('name'), type: eLiteral.eClass.get('name')})
+                        children2.push({
+                            key: getId(eLiteral),
+                            name: eLiteral.get('name'),
+                            type: eLiteral.eClass.get('name')
+                        })
                     }
                 }
                 let eOperations = eClassifier.get('eOperations');
                 if (eOperations) {
                     for (let eOperation of eOperations.array()) {
-                        children2.push({key: getId(eOperation), name: getName(eOperation), type: eOperation.eClass.get('name')})
+                        children2.push({
+                            key: getId(eOperation),
+                            name: getName(eOperation),
+                            type: eOperation.eClass.get('name')
+                        })
                     }
+                }
+                if (children2.length === 0) {
+                    delete child['children']
                 }
             }
         }
---
+++ src/modules/resource.ts
@@ -0,0 +1,149 @@
+import {Ecore} from "ecore";
+
+export class Error {
+    error: string = "Unknown error";
+    message?: string;
+    path?: string;
+    status?: number;
+    timestamp?: string;
+    trace?: string;
+    cause?: any;
+
+    static fromResponce(response: Response): Error {
+        return new Error({error: response.statusText, status: response.status, path: response.url, cause: response})
+    }
+    constructor(init?:Partial<Error>) {
+        Object.assign(this, init);
+    }
+}
+
+export interface IErrorHandler {
+    handleError(error: Error): void;
+}
+
+export class Resource implements IErrorHandler {
+    private static _instance: Resource;
+    private errorHandlers: Array<IErrorHandler>;
+
+    private constructor() {
+        this.errorHandlers = [this];
+    }
+
+    static instance(): Resource {
+        if (!Resource._instance) {
+            Resource._instance = new Resource();
+        }
+        return Resource._instance;
+    }
+
+    private reportError(error: Error): void {
+        this.errorHandlers.forEach(h=>h.handleError(error))
+    }
+
+    addErrorHandler(handler: IErrorHandler) {
+        this.errorHandlers.push(handler)
+    }
+
+    removeErrorHandler(handler: IErrorHandler) {
+        const index = this.errorHandlers.indexOf(handler, 0);
+        if (index > -1) {
+            this.errorHandlers.splice(index, 1);
+        }
+    }
+
+    handleError(error: Error): void {
+        console.log(error)
+    }
+
+    fetchJson(input: RequestInfo, init?: RequestInit): Promise<any> {
+        return this.fetch(input, init).then(response => response.json());
+    }
+
+    fetch(input: RequestInfo, init?: RequestInit): Promise<any> {
+        return fetch(input, init).then(response=>{
+            if (!response.ok) {
+                throw response;
+            }
+            return response
+        }).catch((error) => {
+            if (error instanceof Error) {
+                this.reportError(error)
+            }
+            else if (error instanceof Response) {
+                let response = error as Response;
+                response.json().then(json => {
+                    this.reportError(new Error(Object.assign({}, json, {cause: response})));
+                }).catch(error => {
+                    this.reportError(Error.fromResponce(response));
+                })
+            }
+            else {
+                this.reportError(new Error(Object.assign({}, error, {cause: error})));
+            }
+            return Promise.reject(error)
+        });
+    }
+
+    fetchEObject(ref: string, level: number = 1, resourceSet?: Ecore.ResourceSet, loading?: any): Promise<any> {
+        loading = loading || {}
+        let rs = resourceSet || Ecore.ResourceSet.create();
+
+        let resid = ref.split('#')[0];
+        let refid = resid.split('?')[0];
+
+        if (loading.hasOwnProperty(refid)) {
+            return loading[refid];
+        }
+
+        function collectReferences(object: any, found: string[]): string[] {
+            if (typeof object !== 'object') {
+                return found;
+            }
+            let ref = object.$ref;
+            if (ref) {
+                found.push(ref);
+            }
+            for(var i in object) {
+                if(object.hasOwnProperty(i)){
+                    collectReferences(object[i], found);
+                }
+            }
+            return found;
+        }
+
+        let result =  this.fetchJson(`/emf/object?ref=${refid}`).then(json=>{
+            let refEObjects: Promise<any>[] = []
+            if (level > 0) {
+                let refs = collectReferences(json, []);
+                refEObjects = refs.map(ref=>{
+                    return this.fetchEObject(ref, level - 1, rs);
+                })
+            }
+            return Promise.all(refEObjects).then(resources=>{
+                let resource = rs.create({ uri: resid });
+                resource.load(json);
+                let eObject = resource.get('contents').first();
+                return eObject;
+            })
+        })
+        loading[refid] = result;
+        return result;
+    }
+
+    fetchPackages(): Promise<Ecore.EPackage[]> {
+        return this.fetchJson("/emf/packages").then(json => {
+            Ecore.EPackage.Registry.ePackages().splice(2);
+            let resourceSet = Ecore.ResourceSet.create();
+            for (let aPackage of json as any[]) {
+                let uri = aPackage['nsURI'];
+                let resource = resourceSet.create({uri});
+                resource.load(aPackage);
+                resource.get('contents').each((ePackage: Ecore.EPackage) => {
+                    Ecore.EPackage.Registry.register(ePackage);
+                })
+            }
+            return Ecore.EPackage.Registry.ePackages()
+        })
+    }
+
+}
