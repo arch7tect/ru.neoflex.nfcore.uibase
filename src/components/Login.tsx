2fd33229266a11c61458f4df1d755c27ff3ebe27
hedaira <mezzo666@mail.ru>
2019-07-19 18:24:49 +0300

    delete spring http.httpBasic() correct Login.tsx add Design for Login.tsx add logo2 on LogoPage

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import {Layout} from 'antd';
+import { Layout, Row, Col,Button } from 'antd'
 import {API} from "../modules/api";
-
-const { Content } = Layout;
+import logo2 from '../logo2.png';
+const { Header, Content } = Layout;
 
 export interface Props {
     onLoginSucceed: (principal: any)=>void;
@@ -19,38 +19,53 @@ export class Login extends React.Component<Props, State> {
 
     render() {
         return (
-            <Layout>
-                <Content style={{height: '75vh', backgroundColor: '#ffffff'}}>
-                    <div className='form-div'>
-                        <br/>
-                        <Layout>
-                            <input
-                                autoFocus
-                                className="input-border"
-                                key="user"
-                                placeholder="User Name"
-                                onChange={e => {
-                                    this.setState({userName: e.target.value})
-                                }}
-                                onKeyUp={this.authenticateIfEnterPress}
-                            />
-                            <input
-                                className="input-border"
-                                key="pass"
-                                type="password"
-                                placeholder="password"
-                                onChange={e => {
-                                    this.setState({password: e.target.value})
-                                }}
-                                onKeyUp={this.authenticateIfEnterPress}
-                            />
-                            <button key="conbutton" className="custom-button"
-                                    onClick={this.authenticate}>Sign in
-                            </button>
-                        </Layout>
-                    </div>
-                </Content>
-            </Layout>
+            <div>
+                <Layout>
+                    <Header style={{ height: '30vh', backgroundColor: '#ffffff' }}>
+                        <Row type="flex"  justify="space-between">
+                            <Col span={16}>
+                            {/*<Button style={{height: '27vh', width: '29vh',backgroundColor: '#ffffff'}}>*/}
+                                <img src={logo2} className="logo"/>
+                            {/*</Button>*/}
+                            </Col>
+                            <Col>
+                                <Button type="dashed">
+                                    {/*Настроить переключение языков*/}
+                                    EN
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Header>
+                        <Content style={{height: '65vh', backgroundColor: '#ffffff'}}>
+                            <div className='form-div'>
+                                    <input
+                                        autoFocus
+                                        className="input-border"
+                                        key="user"
+                                        placeholder="User Name"
+                                        onChange={e => {
+                                            this.setState({userName: e.target.value})
+                                        }}
+                                        onKeyUp={this.authenticateIfEnterPress}
+                                    />
+                                    <input
+                                        className="input-border"
+                                        key="pass"
+                                        type="password"
+                                        placeholder="password"
+                                        onChange={e => {
+                                            this.setState({password: e.target.value})
+                                        }}
+                                        onKeyUp={this.authenticateIfEnterPress}
+                                    />
+                                    <button key="conbutton" className="custom-button"
+                                            onClick={this.authenticate}>Sign in
+                                    </button>
+                            </div>
+                        </Content>
+                    {/*<Footer style={{ height: '5vh', backgroundColor: '#ffdedf' }} />*/}
+                </Layout>
+            </div>
         )
     }
 
@@ -59,11 +74,11 @@ export class Login extends React.Component<Props, State> {
             .then((principal)=>{
                 this.props.onLoginSucceed(principal)
             })
-};
+    };
 
     authenticateIfEnterPress = (e:any) => {
         if (e.keyCode === 13) {
             this.authenticate()
-        }};
-
+        }
+    };
 }
