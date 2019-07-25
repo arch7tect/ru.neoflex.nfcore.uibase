5043fb6fe8d839cec2b5bfcb594faddb6685993d
hedaira <mezzo666@mail.ru>
2019-07-25 14:16:12 +0300

    add ref to settings && developer

--- src/EcoreApp.tsx
+++ src/EcoreApp.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Col, Layout, Menu, Row} from 'antd';
+import {Layout, Menu} from 'antd';
 import 'antd/dist/antd.css';
 //import {Ecore} from "ecore";
 import {API} from './modules/api'
@@ -27,8 +27,11 @@ export class EcoreApp extends React.Component<any, State> {
         if (e.key == "logout") {
             API.instance().logout().then(() => {
                 this.setState({principal : undefined});
-
             })
+            this.props.history.push('')
+        }
+        else if (e.key == "developer") {
+            this.props.history.push('/settings/data');
         }
     }
 
@@ -45,54 +48,65 @@ export class EcoreApp extends React.Component<any, State> {
                     </Layout>
                     :
                     <Layout>
-                        {this.renderDef()}
+                        {this.renderDev()}
                     </Layout>
                 }
             </Layout>
         )
     }
 
-    renderDef() {
-        let selectedKeys = ['metadata', 'data', 'query', 'login']
-            .filter(k => this.props.location.pathname.split('/').includes(k));
+    renderDev() {
         let principal = this.state.principal as any;
         return (
             <Layout>
 
-
                 <Header style={{height: '100%', padding: "0px"}}>
-                    {/*<Row>*/}
-                    {/*    <Col>*/}
-                    <Menu mode="horizontal" theme="dark" onClick={(e) => this.onRightMenu(e)} style={{float: "right", height: '40px'}}>
-                            {/*<Menu mode="horizontal" theme="dark" onClick={(e) => this.onRightMenu(e)} style={{float: "right", height: '40px'}}>*/}
-                            <Menu.SubMenu title={<span>{principal.name}</span>}>
-                                <Menu.Item key={'logout'}>Logout</Menu.Item>
-                            </Menu.SubMenu>
+                    <Menu mode="horizontal" theme="dark" onClick={(e) => this.onRightMenu(e)} style={{float: "right"}}>
+                        <Menu.SubMenu title={<span>{principal.name}</span>}>
+                            <Menu.Item key={'logout'}>Logout</Menu.Item>
+                            <Menu.Item key={'developer'}>Developer</Menu.Item>
+                        </Menu.SubMenu>
                     </Menu>
-                        {/*</Col>*/}
-                    {/*</Row>*/}
                 </Header>
 
+                <Switch>
+                    <Route path='/app' component={this.renderStartPage}/>
+                    <Route path='/settings' component={this.renderSettings} />
+                </Switch>
+            </Layout>
+        )
+    }
 
-                <Layout style={{height: '95.9vh'}}>
-                    <Sider collapsible breakpoint="lg" collapsedWidth="0">
-                        <Menu theme="dark" mode="inline" selectedKeys={selectedKeys}>
-                            <Menu.Item key={'metadata'}><Link to={`/metadata`}>Metadata</Link></Menu.Item>
-                            <Menu.Item key={'data'}><Link to={`/data`}>Data</Link></Menu.Item>
-                            <Menu.Item key={'query'}><Link to={`/query`}>Query</Link></Menu.Item>
-                        </Menu>
-                    </Sider>
-                    <Layout>
-                        <Content>
-                            <Switch>
-                                <Route path='/metadata' component={MetaBrowser}/>
-                                <Route exact={true} path='/data' component={DataBrowser}/>
-                                <Route path='/data/:id/:ref' component={ResourceEditor}/>
-                                <Route path='/query' component={QueryRunner}/>
-                            </Switch>
-                        </Content>
-                    </Layout>
+    renderSettings=()=>{
+        let selectedKeys = ['metadata', 'data', 'query']
+            .filter(k => this.props.location.pathname.split('/').includes(k));
+        return (
+            <Layout style={{height: '95.25vh'}}>
+                <Sider collapsible breakpoint="lg" collapsedWidth="0">
+                    <Menu theme="dark" mode="inline" selectedKeys={selectedKeys}>
+                        <Menu.Item key={'metadata'}><Link to={`/settings/metadata`}>Metadata</Link></Menu.Item>
+                        <Menu.Item key={'data'}><Link to={`/settings/data`}>Data</Link></Menu.Item>
+                        <Menu.Item key={'query'}><Link to={`/settings/query`}>Query</Link></Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Content>
+                        <Switch>
+                            <Route path='/settings/metadata' component={MetaBrowser}/>
+                            <Route exact={true} path='/settings/data' component={DataBrowser}/>
+                            <Route path='/settings/data/:id/:ref' component={ResourceEditor}/>
+                            <Route path='/settings/query' component={QueryRunner}/>
+                        </Switch>
+                    </Content>
                 </Layout>
+            </Layout>
+        )
+    }
+
+    renderStartPage() {
+        return (
+            <Layout>
+                App
             </Layout>
         )
     }
